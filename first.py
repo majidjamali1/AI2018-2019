696662af7d7c51b6cbf33b089dd4930e41e5f804
majidjamali1 <55222821+majidjamali1@users.noreply.github.com>
2019-10-31 16:13:07 +0330

    doing BFS!
    just showing Road left! X

--- first.py
+++ first.py
@@ -1,88 +1,112 @@
 import tkinter as tk
 
 
+walls = []
+buttons = []
+
+root = tk.Tk()
+solveAlgRoot = ""
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def enQueue(self, item):
+        self.items.insert(0, item)
+    
+    def deQueue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+ 
 
 def IDS():
     pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def DFS():
     pass
 
 
+def findMoves(row, col):
+
+    moves =  []
+
+    # we start from 0 and end in size-1
+
+    if row < size - 1:
+        # move right
+        right = [row + 1, col]
+        if right not in walls:
+            moves.append(right)
+    
+    if row > 0:
+        # move left
+        left = [row - 1, col]
+        if left not in walls:
+            moves.append(left)
+    
+    if col < size - 1:
+        # move down
+        down = [row, col + 1]
+        if down not in walls:
+            moves.append(down)
+    
+    if col > 0:
+        # move up
+        up = [row, col - 1]
+        if up not in walls:
+            moves.append(up)
+
+    return moves
 
 
+def defineTreeStructure():
+    tree = []
+    for btn in buttons:
+        nums = str.split(btn['title'], ", ")
+        current = [int(nums[0]), int(nums[1])]
+        if current not in walls:
+            moves = findMoves(current[0], current[1])
+            tree.append([current, moves])
+    
+    return tree
 
 
+def findRoad(tree, road, end):
 
+    start = [0, 0]
 
-
-
+    for i in range(0, len(tree)):
+        
+        if tree[i][0] == end:
+            road.append(end)
+            for j in tree[i][1]:
+                if j != start:
+                    return findRoad(tree[0: i], road, j)
+                else:
+                    return road
 
 
 def BFS():
-    pass
+
+    tree = defineTreeStructure()
+    seen = []
+    q = Queue()
+
+    road = findRoad(tree, [], [size - 1, size - 1])
+    print(road)
 
 
+    
 
 
-
-
-
-
-
-
-walls = []
-buttons = []
-
-root = tk.Tk()
-solveAlgRoot = ""
 
 def saveRoadMap():
 
@@ -95,20 +119,20 @@ def saveRoadMap():
     f.close()
 
 
-def showSolveAlgorithms():
+def showSolveAlgorithms(size):
     
     saveRoadMap()
 
     solveAlgRoot = tk.Tk()
 
-    button1 = tk.Button(solveAlgRoot, text="BFS", name="bfs", command=BFS())
+    button1 = tk.Button(solveAlgRoot, text="BFS", name="bfs", command=BFS)
     button1.grid(row=2, column=1, sticky="nsew")
 
-    button2 = tk.Button(solveAlgRoot, text="DFS", name="dfs", command=DFS())
-    button2.grid(row=2, column=3, sticky="nsew")
+    # button2 = tk.Button(solveAlgRoot, text="DFS", name="dfs", command=DFS)
+    # button2.grid(row=2, column=3, sticky="nsew")
     
-    button3 = tk.Button(solveAlgRoot, text="IDS", name="ids", command=IDS())
-    button3.grid(row=3, column=2, sticky="nsew")
+    # button3 = tk.Button(solveAlgRoot, text="IDS", name="ids", command=IDS)
+    # button3.grid(row=3, column=2, sticky="nsew")
 
     solveAlgRoot.grid_rowconfigure(1, weight=1)
     solveAlgRoot.grid_columnconfigure(1, weight=1)
@@ -123,6 +147,11 @@ def showSolveAlgorithms():
 
 def setHereWall(row, col):
     widgetName = "{}, {}".format(row, col)
+
+    if (row == 0 and col ==0) or (row == size-1 and col == size-1):
+        # this is start point or end point! Then
+        return
+
     if [row, col] not in walls:
         walls.append([row, col])
         for i in buttons:
@@ -159,7 +188,7 @@ def createArea(size):
 
         line = file.readline()
 
-    nextStepButton = tk.Button(root, text = "Solve!", name="solvation", command=lambda row=size+2, col=0: showSolveAlgorithms())
+    nextStepButton = tk.Button(root, text = "Solve!", name="solvation", command=lambda row=size+2, col=0: showSolveAlgorithms(size))
     nextStepButton.grid(row=size+2, column=0, sticky="nsew")
 
     root.grid_rowconfigure(size, weight=2)
@@ -176,11 +205,13 @@ def setSize():
     size = input("enter size of maze: (non number for default size)")
 
     try:
-        createArea(int(size))
+        size = int(size)
+        return size
     except:
-        createArea(10)
+        return 10
 
 
 
 if __name__ == "__main__":
-    setSize()
+    size = setSize()
+    createArea(size)
